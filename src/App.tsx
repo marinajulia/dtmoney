9a6062b503d82021133bcf017bef8b71a4db988b
marinajulia <marinajulia707@gmail.com>
2022-06-23 08:21:19 -0300

    chore: Install styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import styled from 'styled-components';
+const Title = styled.h1`
+color: #8257e6;
+font-size: 64px;
+`
 export function App() {
   return (
     <div className="App">
       <header className="App">
         <div>
-          <h1>Teste</h1>
+          <Title>Teste</Title>
         </div>
       </header>
     </div>
