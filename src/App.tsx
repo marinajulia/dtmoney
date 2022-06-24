007ce279e7fdd9d82f7be4d19c4f14b54839df9a
marinajulia <marinajulia707@gmail.com>
2022-06-24 08:40:04 -0300

    chore: Configure Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <header className="App">
-        <div>
-          <h1>teste</h1>
-          <GlobalStyle/>
-        </div>
-      </header>
-    </div>
+    <>
+      <Header/>
+        <GlobalStyle/>
+      </>
   );
 }
