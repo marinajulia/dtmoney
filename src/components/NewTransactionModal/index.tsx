bd5ead7a3ae8b1bcbcfc460c4ca2deaadd9de9ac
marinajulia <marinajulia707@gmail.com>
2022-06-26 08:34:10 -0300

    chore: Initialize styled for modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
 
 interface NewTransactionModalProps{
     isOpen: boolean;
@@ -9,8 +10,17 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
     return(
         <Modal 
         isOpen={isOpen}
-        onRequestClose={onRequestClose}>
-          <h2>Cadastrar transação</h2>
+        onRequestClose={onRequestClose}
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
+        >
+            <Container>
+                <h2>Cadastrar transação</h2>
+                <input placeholder="Título" />
+                <input placeholder="Valor" type="number"/>
+                <input placeholder="Categoria"/>
+                <button type="submit">Cadastrar</button>
+            </Container>
       </Modal>
     );
 }
