0992b90daa047beab9a7f193c1d2b58e94b7228e
marinajulia <marinajulia707@gmail.com>
2022-06-26 08:19:22 -0300

    chore: Refactor modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
-import Modal from 'react-modal';
 import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import Modal from 'react-modal';
 
 Modal.setAppElement('#root');
 
@@ -20,13 +21,10 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard/>
-
-      <Modal 
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
-        onRequestClose={handleCloseNewTransactionModal}>
-          <h2>Cadastrar transação</h2>
-      </Modal>
-
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyle/>
       </>
   );
