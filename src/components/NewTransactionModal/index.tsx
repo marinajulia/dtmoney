02db1fe8006abc13db1a1a54047c27818c39d85b
marinajulia <marinajulia707@gmail.com>
2022-06-26 08:51:58 -0300

    chore: Continue style modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
 import Modal from 'react-modal';
 import { Container } from './styles';
+import close from '../../assets/close.png';
 
 interface NewTransactionModalProps{
     isOpen: boolean;
@@ -14,6 +15,9 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
         overlayClassName="react-modal-overlay"
         className="react-modal-content"
         >
+            <button type="button" onClick={onRequestClose} className="react-modal-close">
+                <img src={close} alt="Fechar modal" />
+            </button>
             <Container>
                 <h2>Cadastrar transação</h2>
                 <input placeholder="Título" />
