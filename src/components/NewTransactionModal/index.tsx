f9d9f34383cd1011ce447524799f5b9c32201a6a
marinajulia <marinajulia707@gmail.com>
2022-06-26 09:11:36 -0300

    chore: Continue style modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
-import { Container } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
 import close from '../../assets/close.png';
+import incomeImg from '../../assets/entradas.svg';
+import outcomeImg from '../../assets/saidas.svg';
 
 interface NewTransactionModalProps{
     isOpen: boolean;
@@ -22,6 +24,19 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
                 <h2>Cadastrar transação</h2>
                 <input placeholder="Título" />
                 <input placeholder="Valor" type="number"/>
+
+                <TransactionTypeContainer>
+                    <button type="button">
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </button>
+
+                    <button type="button">
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Saída</span>
+                    </button>
+                </TransactionTypeContainer>
+
                 <input placeholder="Categoria"/>
                 <button type="submit">Cadastrar</button>
             </Container>
