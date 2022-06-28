ae6d024e99c001db33bd5b62c921a242b319b2c4
marinajulia <marinajulia707@gmail.com>
2022-06-28 08:22:57 -0300

    chore: Save transaction

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -19,15 +19,20 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
     const [category, setCategory] = useState('');
     const [amount, setAmount] = useState(0);
     
-    function handleCreateNewTransaction(event: FormEvent){
+    async function handleCreateNewTransaction(event: FormEvent){
         event.preventDefault();
 
-       createTransaction({
+        await createTransaction({
            title,
            amount,
            category,
            type
-       })
+        })
+        setTitle('');
+        setAmount(0);
+        setCategory('');
+        setType('deposit');
+        onRequestClose();
     }
 
     return(
